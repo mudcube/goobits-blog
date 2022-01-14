6d65f2cd0e1e1afe98b0c9c8689a2216097b36d9
Miko Deal <mudcube@gmail.com>
2022-01-13 23:33:36 -0800

    Initial commit!

---
+++ code/Contact.svelte
@@ -0,0 +1,4 @@
+<title>MIKO.ART - Contact</title>
+
+<h1>Contact Miko 💕</h1>
+<p>Want to collaborate with me?</p>
---
+++ code/index.js
@@ -0,0 +1,3 @@
+import Main from './App.svelte'
+
+new Main({target: document.body})
