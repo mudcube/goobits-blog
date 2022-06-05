69cf0eec929e2a46e166915f5e05c4de5ac2b76e
Miko Deal <mudcube@gmail.com>
2022-06-04 17:31:00 -0700

    Add JS to toggle hamburger

--- src/App.js
+++ src/App.js
@@ -3,3 +3,9 @@ import App from './App.svelte'
 new App({
 	target: document.body
 })
+
+document.querySelector('links').addEventListener('click', () => {
+	if (window.innerWidth < 700) {
+		document.querySelector('links').classList.toggle('open')
+	}
+})
