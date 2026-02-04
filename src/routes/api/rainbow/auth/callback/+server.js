34bc9966f432734649571e477aa99d205472af3e
Miko Meow <101564+mudcube@users.noreply.github.com>
2026-02-03 20:14:00 -0800

    add legacy oauth callback and env override

--- src/routes/api/rainbow/auth/callback/+server.js
+++ src/routes/api/rainbow/auth/callback/+server.js
@@ -1,6 +1,7 @@
-import { onRequest } from '../../../../../../functions/api/rainbow/auth/callback.js'
-import { buildEnv } from '../../_bridge.js'
+import { onRequest } from '../../../../../../functions/api/calendar/calendar/auth/callback.js'
+import { buildEnv } from '../../../calendar/_bridge.js'
 
+// Legacy OAuth callback to support older authorized redirect URIs.
 export async function GET({ request, platform }) {
 	const env = await buildEnv(platform)
 	return onRequest({ env, request })
