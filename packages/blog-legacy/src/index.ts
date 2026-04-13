0b7cb6b3207a14473c8dbb7d404f72576ba20993
Miko Meow <101564+mudcube@users.noreply.github.com>
2026-04-13 16:52:27 -0700

    Remove blog legacy package

--- packages/blog-legacy/src/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as BlogMetadataValues } from './components/MetadataValues.svelte'
-export { default as BlogResults } from './components/Results.svelte'
-export * from './viewmodel'
-export type { JournalPost } from './server/posts'
