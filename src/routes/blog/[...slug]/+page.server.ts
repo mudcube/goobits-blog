365657e508df9c3fa5c1c726cb310ee738cc5f37
Miko Meow <101564+mudcube@users.noreply.github.com>
2026-04-14 12:31:55 -0700

    Add /blog compatibility redirects

---
+++ src/routes/blog/[...slug]/+page.server.ts
@@ -0,0 +1,6 @@
+import { redirect } from '@sveltejs/kit'
+
+export function load({ params }: { params: { slug?: string } }) {
+	const slug = params.slug ? `/${ params.slug }` : ''
+	throw redirect(308, `/journal${ slug }`)
+}
